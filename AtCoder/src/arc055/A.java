6cb8e2711f01aa93908004d31b0a2f60a54ef07b
burnetz <32961116+burnetz@users.noreply.github.com>
2020-04-16 15:09:11 +0900

    ARC055 A AC

---
+++ AtCoder/src/arc055/A.java
@@ -0,0 +1,20 @@
+package arc055;
+
+import java.util.Scanner;
+
+public class A {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		
+		System.out.print(1);
+		for(int i = 0; i < n - 1; i++) {
+			System.out.print(0);
+		}
+		System.out.println(7);
+	}
+
+}
