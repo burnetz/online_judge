63c8af67a24058555e7d9df04e92deb0cd0ef69c
burnetz <32961116+burnetz@users.noreply.github.com>
2020-04-25 13:58:23 +0900

    ARC018 A AC

---
+++ AtCoder/src/arc018/A.java
@@ -0,0 +1,16 @@
+package arc018;
+
+import java.util.Scanner;
+
+public class A {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+
+		double h = sc.nextDouble()/100;
+		double b = sc.nextDouble();
+
+		System.out.println(b*h*h);
+	}
+}
