805730598f13e6b53333b55b3284ecbe70302762
burnetz <32961116+burnetz@users.noreply.github.com>
2020-05-13 15:58:38 +0900

    ABC068 A~C AC(past)

---
+++ AtCoder/src/abc068/B.java
@@ -0,0 +1,21 @@
+package abc068;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class B {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+
+		int i;
+		for(i = 1; i <= n; i *= 2){
+
+		}
+		System.out.println(i/2);
+	}
+
+}
