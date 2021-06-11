cb8da99c5c51ce334ca6e75a6381cab1f3dfc031
burnetz <32961116+burnetz@users.noreply.github.com>
2021-06-12 00:32:39 +0900

    ABC197 A AC

---
+++ AtCoder/src/abc197/A.java
@@ -0,0 +1,16 @@
+package abc197;
+
+import java.util.Scanner;
+
+public class A {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+
+		char[] str = sc.next().toCharArray();
+
+		System.out.println(""+str[1] + str[2] + str[0]);
+	}
+
+}
