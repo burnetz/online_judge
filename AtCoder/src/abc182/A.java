c934ef995658fcdbccc03f595174e975491fe351
burnetz <32961116+burnetz@users.noreply.github.com>
2021-06-13 22:34:39 +0900

    ABC182 A AC

---
+++ AtCoder/src/abc182/A.java
@@ -0,0 +1,17 @@
+package abc182;
+
+import java.util.Scanner;
+
+public class A {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+
+		System.out.println((2*a + 100) - b);
+	}
+
+}
