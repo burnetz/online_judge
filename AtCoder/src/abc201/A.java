6e0837ea0e8ddf1948a9a294b2685d85a9a1f845
burnetz <32961116+burnetz@users.noreply.github.com>
2021-06-15 00:33:15 +0900

    ABC201 A AC

---
+++ AtCoder/src/abc201/A.java
@@ -0,0 +1,23 @@
+package abc201;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class A {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+
+		int a[] = new int[3];
+
+		for(int i = 0; i < 3; i++){
+			a[i] = sc.nextInt();
+		}
+
+		Arrays.sort(a);
+
+		System.out.println(a[1]*2 == a[0] + a[2] ? "Yes" : "No");
+	}
+
+}
