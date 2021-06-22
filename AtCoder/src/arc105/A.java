12bfe8cfcbfd564bc55712b4a2fe926c4c04e176
burnetz <32961116+burnetz@users.noreply.github.com>
2021-06-22 22:03:20 +0900

    ARC105 A AC

---
+++ AtCoder/src/arc105/A.java
@@ -0,0 +1,21 @@
+package arc105;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class A {
+
+	public static void main(String[] args)  {
+		Scanner sc = new Scanner(System.in);
+
+		int a[] = new int[4];
+
+		for(int i = 0; i < 4; i++){
+			a[i] = sc.nextInt();
+		}
+
+		Arrays.sort(a);
+
+		System.out.println((a[0] + a[3] == a[1] + a[2]) || (a[0] + a[1] + a[2] == a[3]) ? "Yes" : "No");
+	}
+}
