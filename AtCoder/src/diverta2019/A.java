376da26bc7f77db1b7fe67f8eb60d229a201abfa
burnetz <32961116+burnetz@users.noreply.github.com>
2021-06-24 14:54:25 +0900

    diverta2019 A AC

---
+++ AtCoder/src/diverta2019/A.java
@@ -0,0 +1,17 @@
+package diverta2019;
+
+import java.util.Scanner;
+
+public class A {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+
+		int n = sc.nextInt();
+		int k = sc.nextInt();
+
+		System.out.println(n - k + 1);
+	}
+
+}
