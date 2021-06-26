a0f8eb582ba18019822c05224b052534a259101e
burnetz <32961116+burnetz@users.noreply.github.com>
2021-06-26 23:41:46 +0900

    indeednow 2015 qualA A AC

---
+++ AtCoder/src/indeednow2015quala/A.java
@@ -0,0 +1,17 @@
+package indeednow2015quala;
+
+import java.util.Scanner;
+
+public class A {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+
+		String s1 = sc.next();
+		String s2 = sc.next();
+
+		System.out.println(s1.length() * s2.length());
+	}
+
+}
