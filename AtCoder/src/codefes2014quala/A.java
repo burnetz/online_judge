a12dc90b2750e111f0ecedf07ded7089b7994c67
burnetz <32961116+burnetz@users.noreply.github.com>
2021-06-29 01:20:15 +0900

    codefes2014 qual a A AC

---
+++ AtCoder/src/codefes2014quala/A.java
@@ -0,0 +1,17 @@
+package codefes2014quala;
+
+import java.util.Scanner;
+
+public class A {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+
+		System.out.println(Math.max(a, b));
+	}
+
+}
