a3d436f7260a363bcc0045e05532fc78cb1caabe
burnetz <32961116+burnetz@users.noreply.github.com>
2021-06-29 00:52:26 +0900

    codefes2015 qual b A AC

---
+++ AtCoder/src/codefes2015qualb/A.java
@@ -0,0 +1,15 @@
+package codefes2015qualb;
+
+import java.util.Scanner;
+
+public class A {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		String s = sc.next();
+
+		System.out.println(s+s);
+	}
+
+}
