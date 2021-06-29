4e3f6e3ef550f736b6e2e5798d96c658276d9ba3
burnetz <32961116+burnetz@users.noreply.github.com>
2021-06-29 21:27:37 +0900

    code formula2014 qual A AC

---
+++ AtCoder/src/codeformula2014qualb/A.java
@@ -0,0 +1,16 @@
+package codeformula2014qualb;
+
+import java.util.Scanner;
+
+public class A {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+
+		int a = sc.nextInt();
+
+		System.out.println(7 - a);
+	}
+
+}
