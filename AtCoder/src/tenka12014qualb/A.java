f70a2a6c5a3ab0649165849f3ff3eb77a9a0ba75
burnetz <32961116+burnetz@users.noreply.github.com>
2021-07-05 23:17:02 +0900

    tenka1_2014 qual b A AC

---
+++ AtCoder/src/tenka12014qualb/A.java
@@ -0,0 +1,18 @@
+package tenka12014qualb;
+
+import java.util.Scanner;
+
+public class A {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+
+		String str = sc.next();
+
+		str = str.replace("HAGIYA", "HAGIXILE");
+
+		System.out.println(str);
+	}
+
+}
