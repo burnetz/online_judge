641b73c68f2eb61dead31dfd779a8247e2cad35a
burnetz <32961116+burnetz@users.noreply.github.com>
2021-07-12 17:52:57 +0900

    donuts2015 A AC

---
+++ AtCoder/src/donuts2015/A.java
@@ -0,0 +1,17 @@
+package donuts2015;
+
+import java.util.Scanner;
+
+public class A {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+
+		int r = sc.nextInt();
+		int d = sc.nextInt();
+		
+		System.out.println(Math.PI*2*d*Math.PI*r*r);
+	}
+
+}
