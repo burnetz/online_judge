56d19bbb87c6c912cdda7fd1d845bdc431201d78
burnetz <32961116+burnetz@users.noreply.github.com>
2022-04-12 16:21:48 +0900

    ABC247 A~C AC

---
+++ AtCoder/src/abc247/A.java
@@ -0,0 +1,16 @@
+package abc247;
+
+import java.util.Scanner;
+
+public class A {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		String str = sc.next();
+		
+		System.out.println("0" + str.substring(0, str.length() - 1));
+	}
+}
+
