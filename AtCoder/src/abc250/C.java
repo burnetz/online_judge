39bab47d9d3de7c391a967b4b55e1a1ad135c75c
burnetz <32961116+burnetz@users.noreply.github.com>
2022-05-10 15:07:07 +0900

    ABC250 C AC

---
+++ AtCoder/src/abc250/C.java
@@ -0,0 +1,50 @@
+package abc250;
+
+import java.util.Scanner;
+
+public class C {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		int q = sc.nextInt();
+		
+		int x[] = new int[n];
+		//それぞれの数字がどこにあるかを記録する配列
+		int index[] = new int[n];
+		
+		for(int i = 0; i < n; i++) {
+			x[i] = index[i] = i;
+		}
+		
+		for(int i = 0; i < q; i++) {
+			int tmp = sc.nextInt() - 1;
+			int tmpIndex = index[tmp];
+			int nextIndex;
+			if(tmpIndex != n - 1) {
+				nextIndex = tmpIndex + 1;
+			}
+			else {
+				nextIndex = tmpIndex - 1;
+			}
+			
+			int swapTmp = x[tmpIndex];
+			x[tmpIndex] = x[nextIndex];
+			index[x[tmpIndex]] = tmpIndex;
+			x[nextIndex] = swapTmp;
+			index[x[nextIndex]] = nextIndex;
+						
+			StringBuffer sb = new StringBuffer();
+			
+		}
+		
+		StringBuffer sb = new StringBuffer();
+		
+		for(int i = 0; i < n; i++) {
+			sb.append((x[i] + 1) + " ");
+		}
+		System.out.println(sb);
+	}
+}
