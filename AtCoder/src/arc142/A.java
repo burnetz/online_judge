bd6b93703d688324d61730802be12049bbb886f7
burnetz <32961116+burnetz@users.noreply.github.com>
2022-06-29 11:47:28 +0900

    ARC142 A AC

--- AtCoder/src/arc142/A.java
+++ AtCoder/src/arc142/A.java
@@ -1,6 +1,6 @@
 package arc142;
 
-import java.util.HashSet;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class A {
@@ -8,59 +8,21 @@ public class A {
 	public static void main(String[] args)  {
 		Scanner sc = new Scanner(System.in);
 
-		long n = sc.nextLong();
-		StringBuffer sb = new StringBuffer(sc.next());
-		long input1 = Long.parseLong(sb.toString());
-		//もともとの入力を左右反転したもの
-		long input2 = Long.parseLong(sb.reverse().toString());
+		long a[] = new long[3];
 		
-		//反転すると小さくなるならどうやってもf(x)=kにはできない
-		if(input1 > input2) {
-			System.out.println(0);
-			return;
-		}
-
-		//1回反転すると左右のいずれにも0がなくなるので
-		//1桁目に0があるような数字を作れるのはその数字自身しかない。
-		if(input1 % 10 == 0) {
-			System.out.println(1);
-			return;
+		for(int i = 0; i < 3; i++) {
+			a[i] = sc.nextLong();
 		}
 		
-		//ここからは重複がありえるのでsetで管理
-		HashSet<Long> set = new HashSet<Long>();
-
-		if(input1 <= n) {
-			set.add(input1);
+		Arrays.sort(a);
+		
+		if(a[2] > a[0] + a[1]) {
+			System.out.println(-1);
+			return;
 		}
-		if(input2 <= n) {
-			set.add(input2);
+		else {
+			System.out.println(a[2]);
 		}
-
-		//ここに来ている時点で最終型の1の位は0ではないので
-		//核となる数字のブロックに0を追加しnより小さいもの全てが答えになる
-		while(true) {
-			input1 *= 10;
-
-			if(input1 <= n) {
-				set.add(input1);
-			}
-			else {
-				break;
-			}
-		}
-
-		while(true) {
-			input2 *= 10;
-
-			if(input2 <= n) {
-				set.add(input2);
-			}
-			else {
-				break;
-			}
-		}
-		System.out.println(set.size());
 	}
 
 }
